0ea62764bdc3fc29df715518e32a9b8ebc70bf6f
HarshitChhipa <harshitkankerwal@gmail.com>
2018-04-07 18:24:57 +0530

    pehla commit

---
+++ Test/datadirectorytest.py
@@ -0,0 +1,154 @@
+import sys
+sys.path.append("../")
+
+import unittest
+
+import Algorithmia
+from Algorithmia.datadirectory import DataDirectory
+from Algorithmia.data import DataObjectType
+from Algorithmia.acl import Acl, AclType
+
+class DataDirectoryTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Algorithmia.client()
+
+    def test_get_name(self):
+        dd = DataDirectory(self.client, 'data://.my/this/is/a/long/path')
+        self.assertEqual('path', dd.getName())
+
+    def test_directory_does_not_exit(self):
+        dd = DataDirectory(self.client, "data://.my/this_should_never_be_created")
+        self.assertFalse(dd.exists())
+
+    def test_alternate_directory_syntax(self):
+        # Check alternate dir syntax
+        dd = self.client.dir("data://.my/this_should_never_be_created")
+        self.assertFalse(dd.exists())
+
+    def test_empty_directory_creation_and_deletion(self):
+        dd = DataDirectory(self.client, "data://.my/empty_test_directory")
+
+        if (dd.exists()):
+            dd.delete(False)
+
+        self.assertFalse(dd.exists())
+
+        dd.create()
+        self.assertTrue(dd.exists())
+
+        # get rid of it
+        dd.delete(False)
+        self.assertFalse(dd.exists())
+
+    def test_nonempty_directory_creation_and_deletion(self):
+        dd = DataDirectory(self.client, "data://.my/nonempty_test_directory")
+
+        if (dd.exists()):
+            dd.delete(True)
+
+        self.assertFalse(dd.exists())
+
+        dd.create()
+        self.assertTrue(dd.exists())
+
+        f = dd.file('one')
+        self.assertFalse(f.exists())
+        f.put('data')
+        self.assertTrue(f.exists())
+
+        # Try deleting without the force - the directory should still be there
+        self.assertRaises(Exception, dd.delete)
+        self.assertTrue(dd.exists())
+        self.assertTrue(f.exists())
+
+        dd.delete(True)
+        self.assertFalse(dd.exists())
+        self.assertFalse(f.exists())
+
+    def list_files_small(self, collectionName):
+        dd = DataDirectory(self.client, collectionName)
+        if (dd.exists()):
+            dd.delete(True)
+
+        dd.create()
+
+        f1 = dd.file('a')
+        f1.put('data')
+
+        f2 = dd.file('b')
+        f2.put('data')
+
+        size = 0
+        all_files = set()
+        for f in dd.files():
+            all_files.add(f.path)
+            size += 1
+
+        self.assertEqual(2, size)
+        self.assertTrue('.my/test_list_files_small/a' in all_files)
+        self.assertTrue('.my/test_list_files_small/b' in all_files)
+
+        dd.delete(True)
+
+    def test_list_files_small_without_trailing_slash(self):
+        self.list_files_small('data://.my/test_list_files_small')
+
+    def test_list_files_small_with_trailing_slash(self):
+        self.list_files_small('data://.my/test_list_files_small/')
+
+    def test_list_folders(self):
+        dd = DataDirectory(self.client, 'data://.my/')
+
+        dirName = '.my/test_list_directory'
+        testDir = DataDirectory(self.client, 'data://' + dirName)
+        if testDir.exists():
+            testDir.delete(True)
+
+        all_folders = set()
+        for f in dd.dirs():
+            all_folders.add(f.path)
+        self.assertFalse(dirName in all_folders)
+
+        testDir.create()
+        all_folders = set()
+        for f in dd.dirs():
+            all_folders.add(f.path)
+        self.assertTrue(dirName in all_folders)
+
+        testDir.delete(True)
+
+    def test_list_files_with_paging(self):
+        NUM_FILES = 1100
+        EXTENSION = '.txt'
+
+        dd = DataDirectory(self.client, 'data://.my/pythonLargeDataDirList')
+        if not dd.exists():
+            dd.create()
+
+            for i in range(NUM_FILES):
+                dd.file(str(i) + EXTENSION).put(str(i))
+
+        seenFiles = [False] * NUM_FILES
+        numFiles = 0
+
+        for f in dd.files():
+            numFiles += 1
+            name = f.getName()
+            index = int(name[:-1 * len(EXTENSION)])
+            seenFiles[index] = True
+
+        allSeen = True
+        for cur in seenFiles:
+            allSeen = (allSeen and cur)
+
+        self.assertEqual(NUM_FILES, numFiles)
+        self.assertTrue(allSeen)
+
+    def test_data_object(self):
+        dd = DataDirectory(self.client, 'data://foo')
+        self.assertTrue(dd.is_dir())
+        self.assertFalse(dd.is_file())
+        self.assertTrue(dd.get_type() is DataObjectType.directory)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Test/utiltest.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append("../")
+
+import unittest
+
+import Algorithmia
+from Algorithmia.util import getParentAndBase, pathJoin
+
+class UtilTest(unittest.TestCase):
+    def test_getParentAndBase(self):
+        self.assertEqual(('a/b', 'c'), getParentAndBase('a/b/c'))
+        self.assertEqual(('data://foo', 'bar'), getParentAndBase('data://foo/bar'))
+        self.assertEqual(('data:///', 'foo'), getParentAndBase('data:///foo'))
+        self.assertEqual(('data://', 'foo'), getParentAndBase('data://foo'))
+
+    def test_getParentAndBase_errors(self):
+        self.assertRaises(Exception, getParentAndBase, '/')
+        self.assertRaises(Exception, getParentAndBase, '')
+        self.assertRaises(Exception, getParentAndBase, 'a/')
+
+    def test_pathJoin(self):
+        self.assertEqual('/a/b/c/d', pathJoin('/a/b/c/', 'd'))
+        self.assertEqual('/a/b/c/d', pathJoin('/a/b/c', 'd'))
+        self.assertEqual('/a//b/c///d', pathJoin('/a//b/c//', '/d'))
+
+if __name__ == '__main__':
+    unittest.main()
